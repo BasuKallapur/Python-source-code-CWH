550e1c6c7fe3f33bbf728ab00563677ce85aac5f
BasuKallapur <basukallapur0@gmail.com>
2024-08-30 08:06:31 +0530

    added files

---
+++ module.py
@@ -0,0 +1,7 @@
+def myFunc():
+    print("hello world!")
+
+if __name__ == "__main__":
+    # if the code is directly excecuted by running the file present in 
+    myFunc()    
+    print(__name__)
